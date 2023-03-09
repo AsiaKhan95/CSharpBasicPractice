8ce30773e933bb0e26741c8189f7139511d1a193
DELL <DELL@DESKTOP-TCUDTKI>
2023-03-09 18:40:41 +0500

    Add project files.

---
+++ CSharpBasicPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Principal;
+
+namespace CSharpBasicPractice
+{
+    public class Program
+    {
+        public enum Role
+        {
+            Admin,
+            Editor,
+            Author
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.Clear();
+            if (name == "Asia")
+            {
+                Console.WriteLine($"Wellcom {name} ");
+            }
+            else {
+                Console.WriteLine("Sorry Access Denied...");
+            }
+        }
+    }
+}
